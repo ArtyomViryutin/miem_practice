fad1d4a90c74679131667b631a66f8170cf39e33
Artyom <virytinart1@gmail.com>
2021-07-18 16:12:23 +0300

    change description

--- match.py
+++ match.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 from file_parser import parse_docx, parse_xls
-from tools import find_row_matches, dump_to_excel
+from utils import find_row_matches, dump_to_excel
 import argparse
 from pathlib import Path
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Парсинг .doc(x) и .xls(x)')
+    parser = argparse.ArgumentParser(description='Поиск сравнений между .dic(x) и .xls(x) файлами')
     parser.add_argument('-p', '--path', required=True, metavar=None,
                         help='Абсолютный или относительный путь к папке с .doc(x) и .xls(x) файлами')
     parser.add_argument('-up', '--upload-path', default=Path(__file__).parent.absolute(), required=False,
